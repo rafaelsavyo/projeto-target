b3fba350d5abf678b457145dda8b4483a04bc2cf
rafaelsavyo <rafaelblg.rb@gmail.com>
2024-09-10 19:36:07 -0300

    1target.py

--- 1target.py
+++ 1target.py
@@ -1,9 +1,28 @@
-def fibo(num):
+def fibonacciseq(limite):
     sequencia = [0, 1]
     while True:
         valor = sequencia[-1] + sequencia[-2]
-        if valor > num:
+        if valor > limite:
             break
         sequencia.append(valor)
     return sequencia
-print(fibo(22))
+
+def fibonaccinum(num):
+    if num < 0:
+        return False
+    sequencia = fibonacciseq(num)
+    return num in sequencia
+
+def prin():
+    try:
+        num = int(input("Informe um número: "))
+        if fibonaccinum(num):
+            print(f"O número {num} pertence à sequência de Fibonacci.")
+        else:
+            print(f"O número {num} NÃO pertence à sequência de Fibonacci.")
+    except ValueError:
+        print("Por favor, insira um número inteiro válido.")
+
+nome = 'teste'
+if nome == "teste":
+    prin()
